b82c15882f89f228c75566aa847d7242888b9cc2
minhpg <minhpg@minhpg-mac-mini.lan>
2021-05-25 12:15:32 +0700

    commit

--- tiny4k.py
+++ tiny4k.py
@@ -48,7 +48,7 @@ class Video:
         return video
 
     def uploadDrive(self):
-        self.file_name = utilities.randomName()
+        self.file_name = utilities.randomName()+'.mp4'
         utilities.download(self.videos[-1]['url'],self.file_name,COOKIE)
         file_id = gDriveLib.upload(self.file_name,self.title+'.mp4',DRIVE,TEAM_DRIVE)
         os.remove(self.file_name)
